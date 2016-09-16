5bb6ea439284111fa96dda798d8afb93fd47ebea
Evan Grossman <grossmae@gmail.com>
2016-09-16 14:33:57 -0700

    Add SMErrorAlertFactory and necessary strings

---
+++ SurpriseMe/SMErrorAlertFactory.swift
@@ -0,0 +1,13 @@
+//
+//  SMErrorAlertFactory.swift
+//  SurpriseMe
+//
+//  Created by Evan Grossman on 9/16/16.
+//  Copyright © 2016 Evan Grossman. All rights reserved.
+//
+
+import UIKit
+
+class SMErrorAlertFactory: NSObject {
+
+}
