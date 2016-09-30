7bd21e8cc5e086e87146366915fdadb510cbab94
Evan Grossman <grossmae@gmail.com>
2016-09-29 23:20:20 -0700

    Update SMViewController for swift3 fix keyboard notification system

--- SurpriseMe/SMViewController.swift
+++ SurpriseMe/SMViewController.swift
@@ -15,78 +15,82 @@ class SMViewController: UIViewController {
     let scrollView = UIScrollView()
     let contentView = UIView()
     let loadingView = UIView()
-    let loadingIndicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.WhiteLarge)
+    let loadingIndicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.whiteLarge)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor.whiteColor()
+        view.backgroundColor = UIColor.white
         self.automaticallyAdjustsScrollViewInsets = false
         
+        NotificationCenter.default.addObserver(self, selector: #selector(SMViewController.keyboardWasShown(_:)), name:.UIKeyboardWillShow, object: nil);
+        NotificationCenter.default.addObserver(self, selector: #selector(SMViewController.keyboardWillHide(_:)), name:.UIKeyboardWillHide, object: nil);
+        
         view.addSubview(scrollView)
-        scrollView.snp_makeConstraints { (make) -> Void in
-            make.top.equalTo(self.snp_topLayoutGuideBottom)
+        scrollView.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(topLayoutGuide.snp.bottom)
             make.left.right.equalTo(0)
-            make.bottom.equalTo(self.snp_bottomLayoutGuideTop)
+            make.bottom.equalTo(bottomLayoutGuide.snp.top)
         }
         
         scrollView.addSubview(contentView)
-        contentView.snp_makeConstraints { (make) -> Void in
+        contentView.snp.makeConstraints { (make) -> Void in
             make.edges.equalTo(scrollView)
             make.width.equalTo(view)
         }
         
         view.addSubview(loadingView)
-        loadingView.snp_makeConstraints { (make) -> Void in
+        loadingView.snp.makeConstraints { (make) -> Void in
             make.top.equalTo(0)
             make.bottom.equalTo(0)
             make.left.equalTo(0)
             make.right.equalTo(0)
         }
-        loadingView.backgroundColor = UIColor.darkGrayColor().colorWithAlphaComponent(0.7)
+        loadingView.backgroundColor = UIColor.darkGray.withAlphaComponent(0.7)
         
         loadingView.addSubview(loadingIndicator)
-        loadingIndicator.snp_makeConstraints { (make) -> Void in
+        loadingIndicator.snp.makeConstraints { (make) -> Void in
             make.center.equalTo(loadingView)
         }
         
-        loadingView.hidden = true
+        loadingView.isHidden = true
     }
     
-    override func viewWillAppear(animated: Bool) {
+    override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
     
     func setBottomView(bottomView: UIView) {
-        contentView.snp_updateConstraints { (make) -> Void in
+        contentView.snp.updateConstraints { (make) -> Void in
             make.bottom.equalTo(bottomView)
         }
     }
     
     func startLoading() {
         loadingIndicator.startAnimating()
-        view.bringSubviewToFront(loadingView)
-        loadingView.hidden = false
+        view.bringSubview(toFront: loadingView)
+        loadingView.isHidden = false
     }
     
     func stopLoading() {
-        loadingView.hidden = true
+        loadingView.isHidden = true
         loadingIndicator.stopAnimating()
     }
     
-    func keyboardWasShown(notification: NSNotification) {
-        
-        let info: NSDictionary = notification.userInfo!
-        let keyboardBeg = info.objectForKey(UIKeyboardFrameBeginUserInfoKey)?.CGRectValue
-        let keyboardSize = keyboardBeg!.size
-        
-        let contentInsets = UIEdgeInsetsMake(0, 0, keyboardSize.height + 75, 0)
-        scrollView.contentInset = contentInsets;
+    func keyboardWasShown(_ notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height, right: 0)
+            scrollView.contentInset = contentInsets;
+        }
     }
     
-    func keyboardWillBeHidden(notification: NSNotification) {
-        let contentInsets = UIEdgeInsetsZero;
+    func keyboardWillHide(_ notification: NSNotification) {
+        let contentInsets = UIEdgeInsets.zero;
         scrollView.contentInset = contentInsets;
         scrollView.scrollIndicatorInsets = contentInsets;
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
 }
