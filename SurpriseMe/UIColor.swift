bddc443b8037d21308e03014435ecce711775ffc
Evan Grossman <grossmae@gmail.com>
2016-10-28 18:13:18 -0700

    Add more colors

--- SurpriseMe/UIColor.swift
+++ SurpriseMe/UIColor.swift
@@ -16,9 +16,33 @@ extension UIColor {
         }
     }
     
+    open class var smLightBlue: UIColor {
+        get {
+            return UIColor(red: 0.78, green: 1.0, blue: 1.0 , alpha: 1.0)
+        }
+    }
+    
+    open class var smDarkBlue: UIColor {
+        get {
+            return UIColor(red: 0.05, green: 0.21, blue: 0.33 , alpha: 1.0)
+        }
+    }
+    
+    open class var smYellow: UIColor {
+        get {
+            return UIColor(red: 1.0, green: 0.83, blue: 0.19 , alpha: 1.0)
+        }
+    }
+    
+    open class var smGray: UIColor {
+        get {
+            return UIColor(red: 1.0, green: 0.83, blue: 0.19 , alpha: 1.0)
+        }
+    }
+    
     open class var smDarkText: UIColor {
         get {
-            return UIColor(red: 0.29, green: 0.29, blue: 0.29 , alpha: 1.0)
+            return UIColor(red: 0.60, green: 0.60, blue: 0.60 , alpha: 0.60)
         }
     }
 
