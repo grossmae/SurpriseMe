1ce6872a0395f762d3a99f6428a48df1e6d929b3
Evan Grossman <grossmae@gmail.com>
2016-11-07 11:56:45 -0800

    Set application tint to SMDarkBlue

--- SurpriseMe/AppDelegate.swift
+++ SurpriseMe/AppDelegate.swift
@@ -20,6 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
+        window?.tintColor = .smDarkBlue
+        
         let rootVC = SearchViewController()
         let navVC = UINavigationController(rootViewController: rootVC)
         
